33124bf3ae300603fb5d9685a999888c0305f6c0
ObviouslyGreen <shibo.yao@gmail.com>
2015-11-17 20:12:33 -0600

    Added logic for hazard detection
    pls work

--- modules/hazard_detector.sv
+++ modules/hazard_detector.sv
@@ -11,6 +11,27 @@ module hazard_detector
     output logic bubble_enable
 );
 
-// TODO
+logic [1:0] counter;
+
+always_ff @(posedge clk)
+begin
+    if (mem_read || (opcode == op_br) || (opcode == op_jmp) || (opcode == op_jsr))
+    begin
+        bubble_enable <= 1'b1;
+        counter <= 2'b00;
+    end
+    else
+    begin
+        if ((counter != 2'b11) && ((opcode == op_br) || (opcode == op_jmp) || (opcode == op_jsr)))
+        begin
+            counter += 1;
+        end
+        else if ((mem_read == 1'b0) || ((counter == 2'b11) && ((opcode == op_br) || (opcode == op_jmp) || (opcode == op_jsr))))
+        begin
+            bubble_enable <= 1'b0;
+            counter <= 2'b00;
+        end
+    end
+end
 
 endmodule : hazard_detector
